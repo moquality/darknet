7768f68226fbeb4bdcd1a44c8c9dde54e44f6dca
Pushkar Kolhe <pushkarkolhe@gmail.com>
2017-02-23 09:38:14 -0500

    Experiment runs with 10 classes but less resolution

--- run_floppy.sh
+++ run_floppy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./darknet detector train ../image_compare/deep_template_matching/data/floppy.data cfg/yolo-voc.cfg backup_2.5_centered/yolo-voc_36000.weights > out.txt 2>&1
+./darknet -i 2 detector train ../image_compare/deep_template_matching/data/floppy.data cfg/yolo-voc.cfg backup_2.5_centered/yolo-voc_36000.weights > out_2.txt 2>&1
 
